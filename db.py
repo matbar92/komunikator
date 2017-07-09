349e8795f64782668e728f02dbb7cbab255c81ec
mateuszbaranowski <brajano92@gmail.com>
2017-07-09 14:39:25 +0200

    test connection

---
+++ db.py
@@ -0,0 +1,18 @@
+from mysql.connector import connect
+
+def connect_to_db(): #polaczenie cnx i cursor
+    cnx = connect(
+        user='root',
+        password='coderslab',
+        database='communicator_db'
+    );
+
+    cursor = cnx.cursor()
+
+    return cnx, cursor
+
+
+def close_connection(cnx, cursor):
+    cursor.close()
+    cnx.close()
+
